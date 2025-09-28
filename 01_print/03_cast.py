126eb29d145367092887b05deced420ae7cf6a77
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-09-28 12:21:34 +0100

    Transformacion de datos

---
+++ 01_print/03_cast.py
@@ -0,0 +1,22 @@
+###
+# 03 - casting de types()
+# Transformar un tipo de un valor a otro
+###
+
+print ("conversion de tipos")
+
+# print ( 2 + int("100"))
+# print ( str(2) + "100")
+
+# print( type(float("3.1416")) )
+# print (int(3.1416))
+
+# print (bool(3))
+# print(bool(0))
+# print(bool(-1))
+
+# print(bool(""))
+# print(bool(" "))
+# print (bool("false"))
+
+   
