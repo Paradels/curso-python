69374cebe4354c447563b5ff9366278b508e47ea
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-09-28 11:48:16 +0100

    Tipos de datos

--- 01_print/02_types.py
+++ 01_print/02_types.py
@@ -5,3 +5,31 @@
 ###
 
 
+print("int:")
+# print(type(10))
+# print(0)
+# print(-5)
+# print(23123123123122141423546765)
+
+print("float:")
+# print(type(10.5))
+# print(0.0)
+# print(-5.67)
+# print(3.4e10)
+
+print("complex:")
+# print(type(3 + 5j))
+
+print("str:")
+# print(type("Hola Mundo"))
+# print(type('Otra forma de definir un string'))
+# print("Un string puede tener números 12345 y símbolos !@#$%&/()=?")
+
+prinyt("bool:")
+# print(type(True))
+# print(type(False))
+# print(10 > 5)
+
+print("NoneType:")
+# print(type(None)) # Representa la ausencia de valor
+
