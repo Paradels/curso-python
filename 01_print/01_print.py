b307e61eb59e442aa65f93c7c1a99581d71bdba2
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-09-28 11:38:29 +0100

    Tipos de print

--- 01_print/01_print.py
+++ 01_print/01_print.py
@@ -1,1 +1,10 @@
+## 01-print 
 print("Hola Mundo")
+print('Una comilla tambien funciona')
+print (  "python", "es", "genial" )
+print (  "python", "es", "genial", sep = "-")
+
+print ( "Esto se imprime", end = " ")
+print ( "en la misma linea" )
+
+
---
+++ 01_print/02_types.py
@@ -0,0 +1,7 @@
+# 01 - Types()
+# Tipos de datos en Python
+# https://docs.python.org/3/library/stdtypes.html
+# int, float, complex, str, bool, NoneType, list, tuple, dict, range, set ...
+###
+
+
