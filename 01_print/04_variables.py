25334246b2bc74bf12e04a5295ba180c425a0a9f
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-09-28 13:04:32 +0100

    Variables

---
+++ 01_print/04_variables.py
@@ -0,0 +1,28 @@
+##
+# 04 - Variables
+# Las variables serven para guardar datos y reutilizarlos a lo largo del código.
+###
+
+my_name = "Luis Paradela"
+
+print(my_name)
+
+age = 27
+print(age)  
+
+age = age + 1
+print(age)
+
+# Tipado dinámico: el tipo de dato se determina en tiempo de ejecución (  tipado dinamico )
+# que no tienes que declarar el tipo de dato de la variable
+name = "Luis"
+print(type(name))
+name = 27
+print(type(name))
+
+# Tipado fuerte: no se pueden mezclar tipos de datos sin convertirlos explícitamente ( tipado fuerte )
+# print (10 + "2")
+
+#f-strings ( formatted strings ) (literal de cadena de formato)
+print(f"Hola {my_name}, tienes {age } años.")
+
